984ba0fec0a83cdf264db4737e3e5740aa2f9cb9
Michanowski9 <michalsoja.judo@gmail.com>
2023-10-29 10:29:15 +0000

    finished pojazd witch closures

--- src/Pojazd.js
+++ src/Pojazd.js
@@ -7,15 +7,15 @@ module.exports = function(id, max_predkosc, predkosc){
     };
 
     this.Start = function(new_predkosc){
-        /*if (new_predkosc <= max_predkosc) {
+        if (new_predkosc < max_predkosc) {
             predkosc = new_predkosc;
         }
         else{
             predkosc = max_predkosc;
-        }*/
+        }
     };
 
     this.Stop = function(){
-        //predkosc = 0;
+        predkosc = 0;
     };
 }
--- test/PojazdTest.js
+++ test/PojazdTest.js
@@ -3,12 +3,6 @@ var Pojazd = require('../src/Pojazd');
 
 describe('pojazd-test', function()
 {
-    it('SettingInConstructor_GetStatus_ReturnsCorrectValue', function()
-    {
-        var pojazd = new Pojazd(0,10, 2);
-        expect(pojazd.GetStatus()).to.eql(2);
-    });
-
     it('SettingInConstructor_UsePrivateVariable_ThrowError', function()
     {
         var pojazd = new Pojazd(0,10, 2);
@@ -30,5 +24,37 @@ describe('pojazd-test', function()
         ).to.throw();
     });
 
+    it('SettingInConstructor_GetStatus_ReturnsCorrectValue', function()
+    {
+        var pojazd = new Pojazd(0,10, 2);
+        expect(pojazd.GetStatus()).to.eql(2);
+    });
 
+    it('PredkoscCorrectValueBySetter_GetStatus_ReturnsCorrectValue', function()
+    {
+        var pojazd = new Pojazd(0,10, 0);
+
+        pojazd.Start(2);
+
+        expect(pojazd.GetStatus()).to.eql(2);
+    });
+
+    it('PredkoscHigherThanMaximumValueBySetter_GetStatus_ReturnsMaximumValue', function()
+    {
+        const MAXIMUM = 10;
+        var pojazd = new Pojazd(0, MAXIMUM, 0);
+
+        pojazd.Start(15)
+
+        expect(pojazd.GetStatus()).to.eql(MAXIMUM);
+    });
+
+    it('Stop_GetStatuts_ReturnZero', function()
+    {
+        var pojazd = new Pojazd(0, 10, 2);
+
+        pojazd.Stop()
+
+        expect(pojazd.GetStatus()).to.eql(0);
+    });
 });
